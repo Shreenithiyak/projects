2e9f9cd23f7602023060d8d6b7141de84460bbf2
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-04-01 17:46:01 +0530

    update

--- reactrouters/src/pages/Optimizationphase.jsx
+++ reactrouters/src/pages/Optimizationphase.jsx
@@ -8,7 +8,7 @@ const Optimizationphase = () => {
   return (
     <>
      <div className="flex flex-col gap-6 items-center bg-mauve-700 h-screen p-10">
-       <h1>Optimization phase</h1>
+       <h1 className="text-2xl font-bold">Optimization phase</h1>
     <button onClick = {()=>navigate("/Task1")}className="px-4 py-2 text-white bg-green-500">Task1 </button>
 
      </div>
